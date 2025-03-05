eb76d853a689006bb73a6494252a7582c93d3ecb
AlenKir <kirillova.alena@gmail.com>
2025-03-05 07:32:27 +0100

    feat: date format

--- ads_api/ads/serializers.py
+++ ads_api/ads/serializers.py
@@ -3,6 +3,9 @@ from .models import Ad
 
 
 class AdSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%B %d, %Y, %I:%M %p")
+    updated_at = serializers.DateTimeField(format="%B %d, %Y, %I:%M %p")
+
     class Meta:
         model = Ad
         fields = '__all__'
