2208da580250561c484b47a4c609585c186ee2a0
AlenKir <kirillova.alena@gmail.com>
2025-04-03 14:57:13 +0200

    refactor: switch to gen views

--- ads_api/ads/urls.py
+++ ads_api/ads/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import get_ads, get_ad, create_ad
+from .views import AdListView, AdDetailView, AdCreateView
 
 urlpatterns = [
-    path('ads/', get_ads, name='get_ads'),
-    path('ads/<int:pk>/', get_ad, name='get_ad'),
-    path('ads/create/', create_ad, name='create_ad'),
+    path('ads/', AdListView.as_view(), name='ad_list'),
+    path('ads/<int:pk>/', AdDetailView.as_view(), name='ad_detail'),
+    path('ads/create/', AdCreateView.as_view(), name='ad_create'),
 ]
