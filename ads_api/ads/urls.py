bf4b9f6b305298af8e82fc07ee97eeaa4aa9bf7e
AlenKir <kirillova.alena@gmail.com>
2025-04-14 12:15:25 +0200

    feat: delete ad

--- ads_api/ads/urls.py
+++ ads_api/ads/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AdListView, AdDetailView, AdCreateView
+from .views import AdListView, AdDetailView, AdCreateView, AdDeleteView
 
 urlpatterns = [
     path('ads/', AdListView.as_view(), name='ad_list'),
     path('ads/<int:pk>/', AdDetailView.as_view(), name='ad_detail'),
     path('ads/create/', AdCreateView.as_view(), name='ad_create'),
+    path('ads/<int:pk>/delete/', AdDeleteView.as_view(), name='ad_delete'),
 ]
