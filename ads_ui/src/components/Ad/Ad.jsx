a35960665defbda63e66762684a8e11c6425d0a6
AlenKir <kirillova.alena@gmail.com>
2025-05-01 14:36:55 +0200

    styles: clean up

--- ads_ui/src/components/Ad/Ad.jsx
+++ ads_ui/src/components/Ad/Ad.jsx
@@ -3,19 +3,21 @@ import styles from "./Ad.module.css";
 
 const Ad = ({ ad }) => {
     return (
-        <Link to={`/ads/${ad.id}`} className={styles.adCard}>
-            <div className={styles.adCard__header}>
-                <h2>
-                    {ad.title} - {ad.price}€
-                </h2>
-            </div>
-            <div className={styles.adCard__body}>
-                <p>{ad.descr}</p>
-            </div>
-            <div className={styles.adCard__footer}>
-                <small>{ad.created_at}</small>
-            </div>
-        </Link>
+        <article className={styles.adCard}>
+            <Link to={`/ads/${ad.id}`}>
+                <div className={styles.adCard__header}>
+                    <h2>
+                        {ad.title} - {ad.price}€
+                    </h2>
+                </div>
+                <div className={styles.adCard__body}>
+                    <p>{ad.descr}</p>
+                </div>
+                <div className={styles.adCard__footer}>
+                    <small>{ad.created_at}</small>
+                </div>
+            </Link>
+        </article>
     );
 };
 
