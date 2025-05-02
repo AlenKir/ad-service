88c5ad2e41b6db1ac6252782824c55eb05dade11
AlenKir <kirillova.alena@gmail.com>
2025-05-02 17:51:27 +0200

    test precommit test db

--- ads_api/ads_api/settings.py
+++ ads_api/ads_api/settings.py
@@ -89,6 +89,10 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
+
+        'TEST': {
+            'NAME': 'ad_service_test',
+        },
     }
 }
 
